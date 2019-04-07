d92105a08c49b0364847631aeb9beb89e83e5555
Aman <amsingh714@gmail.com>
2019-04-07 16:34:16 -0400

    able to GET targets with specific ID

--- routes/tags-router.js
+++ routes/tags-router.js
@@ -13,4 +13,25 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("tags")
+    .where({ id })
+    .first()
+    .then(tag => {
+      if (tag) {
+        res.status(200).json(tag);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The tag with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The tag with the specified ID could not be retrieved."
+      });
+    });
+});
+
 module.exports = router;
