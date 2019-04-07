788b13948adec1f3e2157c8a7002b24a5e222426
Aman <amsingh714@gmail.com>
2019-04-07 13:33:40 -0400

    able to GET note with specific id and tags

--- routes/notes-router.js
+++ routes/notes-router.js
@@ -23,4 +23,32 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("notes")
+    .where({ id })
+    .first()
+    .then(note => {
+      if (note) {
+        db("tags")
+          .where({ note_id: id })
+          .then(tagsArr => {
+            let tags = tagsArr.map(tagObj => tagObj.tags);
+            note.tags = tags;
+            res.status(200).json(note);
+          });
+      } else {
+        res
+          .status(404)
+          .json({ error: "The note with the specified ID does not exist" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The note with the specified ID could not be retrieved."
+      });
+    });
+});
+
 module.exports = router;
